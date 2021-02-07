66a904588f79a947ffcdbe874b052e2dd744fcdc
fuchun198435163.com <tank198435@163.com>
2021-02-07 11:20:49 +0800

    [fuchun] #refactor exclude mvc error template

--- ms-stock/src/main/java/com/tank/msstock/StockApp.java
+++ ms-stock/src/main/java/com/tank/msstock/StockApp.java
@@ -2,13 +2,14 @@ package com.tank.msstock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author tank198435163.com
  */
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class StockApp {
 
   public static void main(final String[] args) {
